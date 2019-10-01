1af2d74439304065ca526c39eb194ba84197ce05
marwaybs <bsmarway@gmail.com>
2019-10-01 14:39:13 -0400

    adding technolgies page

--- app/containers/HomePage/Technologies.js
+++ app/containers/HomePage/Technologies.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
+import H1 from '../../components/H1'
 
 export default class Technologies extends React.PureComponent {
+  
 
   render() {
     const {  } = this.props;
 
     return (
       <div>
-        <h1>Technologies</h1>
-        <span>Technologies</span>
+        <H1>Technologies</H1>
+        <span>React</span>
+        <span>Ruby on Rails</span>
+        <span>Python</span>
+        <span>Others:</span>
+
       </div>
     )
   }
